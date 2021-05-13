830f78fdd44e9729f7b6584d8bbd6a92a072d973
terakawaakari <terakawaakari0701@gmail.com>
2021-05-13 06:29:27 +0000

    [Finish]3章

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,8 +6,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
     assert_select "title","Home | #{@base_title}"
   end
@@ -22,6 +22,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title","About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title","Contact | #{@base_title}"
   end
 
 end
